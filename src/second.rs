c071640cc833926e9dc7d0426b2b92c34892e425
dugi <dugi@channel.io>
2023-01-30 07:59:33 +0900

    refactor: make Link an Option

---
+++ src/second.rs
@@ -0,0 +1,77 @@
+#[derive(Debug)]
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+#[derive(Debug)]
+pub struct List<T> {
+    head: Link<T>,
+}
+
+impl<T> List<T> {
+    /// Creates an empty [`List`].
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    /// Adds an element to the front of the list.
+    pub fn push(&mut self, elem: T) {
+        let new_node = Node {
+            elem,
+            next: core::mem::replace(&mut self.head, None),
+        };
+
+        self.head = Some(Box::new(new_node));
+    }
+
+    /// Removes the first element from the list and returns it, or [`None`] if it is empty.
+    pub fn pop(&mut self) -> Option<T> {
+        match core::mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = core::mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = core::mem::replace(&mut boxed_node.next, None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
